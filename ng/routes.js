6f977d1637bba4180b655b698a5a6fb82e64c4e4
Vishal R <vishivish18@gmail.com>
2015-09-12 22:14:18 +0530

    HTML 5 pushstate to remove #

--- ng/routes.js
+++ ng/routes.js
@@ -1,12 +1,14 @@
 angular.module('app')
-.config(function($routeProvider) {
+.config(function($routeProvider,$locationProvider) {
 	$routeProvider
-	.when('/',{controller:'LoginCtrl',templateUrl:'login.html'})	
+	.when('/',{controller:'LoginCtrl',templateUrl:'/login.html'})	
 	.when('/posts',{controller:'PostsCtrl',templateUrl:'posts.html'})
 	.when('/register',{controller:'RegisterCtrl',templateUrl:'register.html'})
 	.when('/home',{controller:'HomeCtrl',templateUrl:'users/home.html'})	
 	.when('/vehicles/new/info',{controller:'VehiclesNewInfoCtrl',templateUrl:'vehicles/new/info.html'})	
 	.when('/vehicles/edit/:deviceId/info',{controller:'VehiclesEditInfoCtrl',templateUrl:'vehicles/edit/info.html'})	
 	.when('/401',{controller:'ErrorCtrl',templateUrl:'errors/401.html'})	
+
+	$locationProvider.html5Mode(true)
 	
 })
