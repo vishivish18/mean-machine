cc71c84a7ee17a61508ad2270438a680f83594bb
Vishal Ranjan <vishivish18@gmail.com>
2015-11-14 00:59:39 +0530

    mongo try

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://eroku_krgxbkw9:onlinemongolab_2015@ds053954.mongolab.com:53954/heroku_krgxbkw9',function(){
+mongoose.connect('mongodb://eroku_krgxbkw9:onlinemongo_2015@ds053954.mongolab.com:53954/heroku_krgxbkw9',function(){
 	console.log('mongodb connected');
 })
 
